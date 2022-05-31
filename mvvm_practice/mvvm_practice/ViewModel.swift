1049703fb65997548d6089f8d93ffdcdfea2fdc7
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-31 23:41:00 +0900

    add: 뷰 모델 생성

---
+++ mvvm_practice/mvvm_practice/ViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModel.swift
+//  mvvm_practice
+//
+//  Created by 노주영 on 2022/05/31.
+//
+
+import Combine
+
+//뷰 모델 - 데이터 상태를 가지고 있음
+class ViewModel: ObservableObject{
+    
+    init(){
+        print("ViewModel")
+    }
+    
+}
